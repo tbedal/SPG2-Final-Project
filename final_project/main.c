7cd4eb246cbd335f72b334b5c8a34368ec624485
MilkyIQ <66349361+MilkyIQ@users.noreply.github.com>
2025-11-25 18:13:32 -0600

    Clear main for use in final project

--- final_project/main.c
+++ final_project/main.c
@@ -8,6 +8,13 @@
  * @author Joseph Vesterby
 **/
 
+/*
+ * TODO:
+ * [ ] Standardize header file templates
+ * [ ] Figure out where `#ifndef` should be placed in header files
+ * [ ] Convert function comments to the formatted @return stuff
+ */
+
 /* <----------| INCLUDES |----------> */
 
 #include "adc.h"
@@ -23,28 +30,20 @@
 #include "button.h"
 #include "scan.h"
 
-
 /* <----------| DEFINITIONS |----------> */
-
-// Magic values
-#define CRASH_AVOIDANCE_OFFSET 10
 
 // Initialization values
 #define BAUD_RATE 115200
 
-// TODO: use these for interrupts
+// For use with UART interrupts
 volatile char uart_data;
 volatile char flag;
-
-// Pulled from Lab 10's old main
-volatile int button_num; // Current value of LCD pushbuttons
 
 uint16_t servo_rightBound;
 uint16_t servo_leftBound;
 
 /* <----------| PRIVATE METHODS |----------> */
 
-// TODO: should this be moved into uart.h or stay in main?
 // Sets bot into manual mode and until user exits
 void engageManualMode(oi_t* sensor, scanVector vectors[]);
 
@@ -53,27 +52,24 @@ void engageManualMode(oi_t* sensor, scanVector vectors[]);
 uint8_t main(void)
 {
     // Declare variables
-    oi_t *sensor_data = oi_alloc();
-    scanVector measuredVectors[NUM_SCANS];
+    oi_t *sensorData = oi_alloc();
+    scanVector scanData[NUM_SCANS];
     char puttyMessage[MAX_MESSAGE_LEN];
-    char inputChar = 0;
-    double smallestObjectDistance;
-    double nextTurnDegrees;
-    uint8_t smallestObjectAngle;
+    char userInput = 0;
 
     // Initialize variables
-    oi_init(sensor_data);
+    oi_init(sensorData);
     timer_init();
     adc_init();
     uart_init(BAUD_RATE);
     ping_init();
     servo_init();
-    servo_rightBound = CAL_BOT23_SERVO_R;
-    servo_leftBound = CAL_BOT23_SERVO_L;
-
+    servo_rightBound = BOT23_SERVO_BOUND_R;
+    servo_leftBound = BOT23_SERVO_BOUND_R;
 
     // Uncomment and run to find cybot servo callibration values:
-    /*button_init();
+    /*volatile int button_num;
+    button_init();
     init_button_interrupts();
     lcd_init();
     servo_callibrate();
@@ -81,77 +77,6 @@ uint8_t main(void)
 
     // Update putty once serial connection is successful
     uart_sendStr("Serial connection established.\r\n");
-
-    // Primary autonomous/manual operation loop
-    while (1) {
-        /* <----------| STEP 0: WAIT FOR USER COMMAND |----------> */
-
-        // FIXME: GUI gets mad after faulty instructions
-        do { inputChar = uart_getChar(); } while (inputChar != 't' && inputChar != 'h');
-        if (inputChar == 't') {
-            engageManualMode(sensor_data, measuredVectors);
-            continue;
-        }
-
-        /* <----------| STEP 1: SCAN FIELD |----------> */
-
-        // Perform scan across field and print raw distances and filter noise with rolling average
-        scanField(SCAN_START, SCAN_END, SCAN_INCREMENT, measuredVectors);
-        rollingAverageFilter(measuredVectors, NUM_SCANS, BUFFER_SIZE);
-
-        // Find smallest object in filtered data
-        smallestObjectAngle = findSmallestObject(measuredVectors, NUM_SCANS);
-
-        // Point, turn, and drive to smallest object found
-        servo_move(smallestObjectAngle);
-        smallestObjectDistance = (double)measuredVectors[smallestObjectAngle / SCAN_INCREMENT].pingDistance;
-
-        // Notify client of scan results
-        snprintf(puttyMessage, MAX_MESSAGE_LEN, "Wants to turn %u Degrees, then drive: %.1f cm. Press `h` to continue.\r\n", smallestObjectAngle, smallestObjectDistance);
-        uart_sendStr(puttyMessage);
-
-        /* <----------| STEP 2: WAIT FOR USER COMMAND |----------> */
-
-        do { inputChar = uart_getChar(); } while (inputChar != 't' && inputChar != 'h');
-        if (inputChar == 't') {
-            engageManualMode(sensor_data, measuredVectors);
-            continue;
-        }
-
-        /* <----------| STEP 3: ATTEMPT DRIVE |----------> */
-
-        // Turn and drive to smallest object found in field
-        bot_turnDegrees(sensor_data, BOT_TURN_SPEED, 90.0 - smallestObjectAngle);
-        bot_driveDistancePrecise(sensor_data, BOT_CRUISE_SPEED, smallestObjectDistance - CRASH_AVOIDANCE_OFFSET);
-
-        // Follow collision response protocol if either bumper is hit
-        if (bot_isBumped(sensor_data)) {
-            nextTurnDegrees = sensor_data -> bumpLeft ? -90.0 : 90.0;
-
-            // Update user of collision
-            snprintf(puttyMessage, MAX_MESSAGE_LEN, "Wants to go around object by turning %.1f degrees. Press `h` to execute.\r\n", nextTurnDegrees);
-            uart_sendStr(puttyMessage);
-        }
-        else {
-            // Request new input from user
-            uart_sendChar('\n');
-            continue;
-        }
-
-        /* <----------| STEP 4: WAIT FOR USER COMMAND |----------> */
-
-        do { inputChar = uart_getChar(); } while (inputChar != 't' && inputChar != 'h');
-        if (inputChar == 't') {
-            engageManualMode(sensor_data, measuredVectors);
-            continue;
-        }
-
-        /* <----------| STEP 5: KEEP TURNING TILL NO BUMP |----------> */
-
-        bot_driveDistance(sensor_data, -BOT_CRUISE_SPEED, 5.0);
-        bot_turnDegrees(sensor_data, BOT_TURN_SPEED, nextTurnDegrees);
-        bot_driveDistancePrecise(sensor_data, BOT_CRUISE_SPEED, 10.0);
-        bot_turnDegrees(sensor_data, BOT_TURN_SPEED, -nextTurnDegrees);
     }
 }
 
